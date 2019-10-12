3b9a974fd85ccffc4875fb81c87d92085600c7d4
hetpatel33 <patel.het33@gmail.com>
2019-10-12 23:55:44 +0530

    refactor /app/pages/common/Modals/SearchDependenciesModal/index.js : uses overmind and convert to tsx

---
+++ packages/app/src/app/pages/common/Modals/SearchDependenciesModal/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useOvermind } from 'app/overmind';
+import SearchDependencies from 'app/pages/Sandbox/SearchDependencies';
+
+const SearchDependenciesModal: React.FC = () => {
+  const {
+    actions: {
+      editor: { addNpmDependency },
+    },
+  } = useOvermind();
+
+  return (
+    <SearchDependencies
+      onConfirm={(name, version) => addNpmDependency({ name, version })}
+    />
+  );
+};
+
+export default SearchDependenciesModal;
